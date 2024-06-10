2257838ae10d917884035234ec47d11f95bddf75
Yanzhen Huang <huangyanzhen0108@163.com>
2024-06-10 21:43:34 +0800

    :tada: Init Repo

---
+++ src/main/java/com/yanzhen/HYZ/HyzApplication.java
@@ -0,0 +1,13 @@
+package com.yanzhen.HYZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yanzhen/HYZ/HyzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanzhen.HYZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
